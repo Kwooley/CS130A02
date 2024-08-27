dd20f2dc5ff490b854ceff16b31de886616ede56
kwooley <kwooler@gmail.com>
2024-08-27 11:32:52 -0700

    fixing error on line  7

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     cout << "Hello World" << endl;
-    cout << "Hello World" << endl
+    cout << "Hello World" << endl;
 } // ctrl-s
 
 // to open the terminal ctrl-j
