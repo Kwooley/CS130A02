6d3a1c3bf927ebe50269dcab6e6a2153fbe19635
kwooley <kwooler@gmail.com>
2024-08-27 11:27:26 -0700

    add 2 lines

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main()
 {
     cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
 } // ctrl-s
 
 // to open the terminal ctrl-j
