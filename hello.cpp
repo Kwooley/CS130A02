7ffe5ada654f4cc3dbdd13c1e727435f115525a7
kwooley <kwooler@gmail.com>
2024-08-27 11:27:41 -0700

    add 3 lines

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,9 @@ int main()
     cout << "Hello World" << endl;
     cout << "Hello World" << endl;
     cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
+    cout << "Hello World" << endl;
 } // ctrl-s
 
 // to open the terminal ctrl-j
