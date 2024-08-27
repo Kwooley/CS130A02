3835ad07d01b0dd38897ae729f642e30d11e3fb4
kwooley <kwooler@gmail.com>
2024-08-27 11:28:13 -0700

    delete all hello world

--- hello.cpp
+++ hello.cpp
@@ -3,12 +3,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World" << endl;
-    cout << "Hello World" << endl;
-    cout << "Hello World" << endl;
-    cout << "Hello World" << endl;
-    cout << "Hello World" << endl;
-    cout << "Hello World" << endl;
+    
 } // ctrl-s
 
 // to open the terminal ctrl-j
